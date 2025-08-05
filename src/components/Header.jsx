b98d323f7484d740c88c9ff7b39015787ec6e035
Thatisfish <roobin4321@gmail.com>
2025-08-05 17:00:01 +0800

    0805ntub

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import '../styles/Header.css'
+export default function Header() {
+    return (
+        <header id="topbar">
+        <nav className="navigation">
+            <a href="./index.html">
+                <div className="logo">
+                    <img src="./IMAGES/logo.png" alt="logo"/>
+                </div>
+            </a>
+            <ul className="menu">{/* menu */}
+                <li><a href="">最新消息</a></li>
+                <li><a href="">熱門商品</a></li>
+                <li><a href="">Switch</a></li>
+                <li><a href="">PS系列</a></li>
+                <li><a href="">Xbox系列</a></li>
+            </ul>
+            <ul className="nav-icon">{/* icon */}
+                <li><input type="text" placeholder="想找什麼?" className="search-bar"/></li>
+                <li><a className="icon-search" href="#"></a></li>
+                <li><a className="icon-Shopping_cart" href="#"></a></li>
+                <li><a className="icon-login" href="#"></a></li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
