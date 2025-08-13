9432e1249ae7baad19d792c4d4b8847cd09b579b
licahsun <hsunclan@gmail.com>
2025-08-13 16:54:15 +0800

    0813-yu-改名字

---
+++ src/pages/Shopping_cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shopping_cart = () => {
+  return (
+    <div>Shopping_cart</div>
+  )
+}
+
+export default Shopping_cart
--- src/pages/Trolly.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Trolly = () => {
-  return (
-    <div>Trolly</div>
-  )
-}
-
-export default Trolly
