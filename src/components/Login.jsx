aa0685b7d4c712054437bc1a9d695e47214cf6e2
achi9444 <shark1997329@gmail.com>
2025-08-16 17:18:50 +0800

    0816-C-login_完

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,18 +5,25 @@ const Login = ({ onClose }) => {
     return (
         <>
             <div className="c_backDrop" onClick={onClose}>
+                {/* 阻止點視窗的時候被關閉*/}
                 <div className="c_loginBox" onClick={(e) => { e.stopPropagation() }}>
-                    <button className="c_close-btn" onClick={onClose} aria-label="關閉">
-                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div className="c_close-btn" onClick={onClose} aria-label="關閉" >
+                        <svg width="36" height="36" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M2 2L46 46M2 46L46 2" stroke="black" strokeWidth="3" />
                         </svg>
-                    </button>
+                    </div>
                     <h2>登入</h2>
-                    <form className="c_loginForm" action="" method="post" onSubmit={(e) => { e.preventDefault() }}>
+                    <form className="c_loginForm" action="" method="post" onSubmit={(e) => { e.preventDefault(); onClose(); }}>
                         <input type="text" className="c_username" title="使用者名稱" placeholder="請輸入使用者名稱" required autoFocus />
                         <input type="password" className="c_password" title="密碼欄位" placeholder="請輸入密碼" required />
-                        <p>繼續即表示你同意我們的<a href="#">《服務條款》</a>，並確認你已了解 <a href="#">《隱私權政策》</a></p>
+                        <div className='c_rule'>繼續即表示你同意我們的<a href="#">《服務條款》</a>，<br />並確認你已了解<a href="#">《隱私權政策》</a></div>
                         <button className="c_btn-login" type="submit">登入</button>
+                        <div className='c_try'>
+                            <div className='c_join'>加入我們！<a href="#">註冊</a></div>
+                            <div>
+                                <a className='c_forget' href="#">忘記密碼？</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,29 @@
 import Login from '../components/Login'
+import { useState } from 'react';
+
+
 
 const Register = () => {
+  const [showLogin, setShowLogin] = useState(true);
   return (
-    <div><Login /></div>
+    <>
+      {showLogin && (
+        <Login onClose={() => setShowLogin(false)} />
+      )
+      }
+    </>
   )
 }
 
 export default Register
+
+
+// 要重新觸發的時候用的
+// const [showLogin, setShowLogin] = useState(false);
+
+// return (
+//   <>
+//     <Icon onClick={() => setShowLogin(true)} />
+//     {showLogin && <Login onClose={() => setShowLogin(false)} />}
+//   </>
+// );
