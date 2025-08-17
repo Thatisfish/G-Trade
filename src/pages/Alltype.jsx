bf3dfb9fa0d21e2d7bbc643914a6b74fdb368790
licahsun <hsunclan@gmail.com>
2025-08-17 22:12:10 +0800

    0817_Biki_類別總覽切版&樣式

--- src/pages/Alltype.jsx
+++ src/pages/Alltype.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import '../styles/_Card.scss'
+import Banner from '../images/banner_pokemon.avif'
+import Cards from '../components/Card.jsx'
+import Footer from '../components/Footer.jsx'
+import Nav from '../components/Navbar.jsx'
 
 const Alltype = () => {
   return (
-    <div>Alltype</div>
+    <>
+      <Nav />
+      <div>
+        <img src={Banner} alt="banner" />
+      </div>
+
+      <div className='B_category'>
+        <h2>switch</h2><br/>
+        <button className='B_Console' type='button'>主機</button>
+        <button className='B_Game' type='button'>遊戲</button>
+        <button className='B_Accessories' type='button'>配件</button>
+      </div>
+
+      <div className='B_item'>
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+      </div>
+
+      <Footer />
+    </>
   )
 }
 
