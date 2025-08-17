11dc15399f44508f6dcf8ae0bdb147508f0db098
licahsun <hsunclan@gmail.com>
2025-08-17 23:21:06 +0800

    0817_Biki_類別總覽切版&樣式

--- src/pages/Alltype.jsx
+++ src/pages/Alltype.jsx
@@ -8,29 +8,34 @@ const Alltype = () => {
   return (
     <>
       <Nav />
-      <div>
-        <img src={Banner} alt="banner" />
-      </div>
+      <div className='B_content'>
+        <div className='B_banner'>
+          <img src={Banner} alt="banner" />
+        </div>
 
-      <div className='B_category'>
-        <h2>switch</h2><br/>
-        <button className='B_Console' type='button'>主機</button>
-        <button className='B_Game' type='button'>遊戲</button>
-        <button className='B_Accessories' type='button'>配件</button>
-      </div>
+        <p className='B_name'>switch</p>
+        <div className='B_category'>
+          <button className='B_Categories' type='button'>主機 ()</button>
+          <button className='B_Categories' type='button'>遊戲 ()</button>
+          <button className='B_Categories' type='button'>配件 ()</button>
+        </div>
 
-      <div className='B_item'>
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-        <Cards />
-      </div>
+        <div className='B_item'>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+          <Cards id=''/>
+        </div>
 
+        <div>
+
+        </div>
+      </div>
       <Footer />
     </>
   )
