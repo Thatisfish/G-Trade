0ecd6b0569ecf3bd00f32550587eb339bfa63e23
hanmimi601 <hanmimi601@gmail.com>
2025-08-19 09:33:53 +0800

    0819-y-製作收藏頁面sidebar

--- src/pages/Collect.jsx
+++ src/pages/Collect.jsx
@@ -1,15 +1,19 @@
 import Card from "../components/Card"
 import Sidebar from '../components/Sidebar'
 import '../styles/Collect.scss'
+
 const Collect = () => {
   return (
-    <main id='Y_Collect'>
-       
-       
+    <main id="y_Collect">
+      <Sidebar/>
+      <Card/>
+      <Card/>
+    
+    
       <div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Collect
+export default Collect;
