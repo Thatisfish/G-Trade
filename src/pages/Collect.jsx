f9fad5a86e783b990bc892a3bff410c482427cee
hanmimi601 <hanmimi601@gmail.com>
2025-08-20 13:58:17 +0800

    0820-y-修改蒐藏頁面卡片間距

--- src/pages/Collect.jsx
+++ src/pages/Collect.jsx
@@ -56,17 +56,58 @@ const Collect = () => {
       seller: 'fun**01',
       priceNow: '999',
       size: 'small'
-    }
+    },
+    {
+        id: 6,
+        tag: '限時優惠',
+        category: '配件',
+        image: SWP2,
+        title: 'switch 手把 公司貨 九成新 可面交',
+        seller: 'fun31**56',
+        priceNow: '5000',
+        priceOld: '5500',
+        size: 'medium'
+      },
+      {
+          id: 7,
+          tag: '限時優惠',
+          image: SW2,
+          category: '主機',
+          title: '全新紅藍switch主機 附明星大亂鬥遊戲片全新紅藍switch主機 附明星大亂鬥遊戲片',
+          seller: 'fun31**56',
+          priceNow: '5000',
+          priceOld: '5500',
+          size: 'small'
+        },
+        {
+          id: 8,
+          tag: '全新上架',
+          category: '遊戲',
+          image: DS2CE,
+          title: '死亡擱淺典藏版',
+          seller: 'Ds2CE**250626',
+          priceNow: '1100',
+          size: 'medium'
+        },
+        {
+          id: 9,
+          image: DKB,
+          category: '遊戲',
+          title: '咚奇剛蕉力全開+咚奇剛amiibo',
+          seller: 'paly**56',
+          priceNow: '800',
+          size: 'small'
+        },
   ];
 
   return (
 
-    <main id="y_Collect" className="flex gap-6">
+    <main className="y_Collect">
       {/* 側邊欄 */}
       <Sidebar />
 
       {/* 商品清單 */}
-      <div className="grid grid-cols-3 gap-6 flex-1">
+      <div className="y_cardbox">
         {y_products.map((item) => (
           <Card key={item.id} {...item} />
         ))}
