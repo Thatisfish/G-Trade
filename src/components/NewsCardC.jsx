fb063647d4de9fca55e9b8f927d72b7e957d1f39
Thatisfish <roobin4321@gmail.com>
2025-08-22 10:44:20 +0800

    0822-f-實名認證的連結移動到sidebar

--- src/components/NewsCardC.jsx
+++ src/components/NewsCardC.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const NewsCard = () => {
     return (
         <a href="">
-            <img src={banner01} alt="" />
+            <img src="#" alt="" />
             <div className='newfont'>
                 <h4>全館免運，安心入手不多花！</h4>
                 <p>限時活動開跑！於本平台購買任一商品，即享免運優惠，輕鬆下單、無負擔寄送到家！</p>
