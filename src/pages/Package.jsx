f63e2804823eb8942bab6ada57e82c2d077d4f52
achi9444 <shark1997329@gmail.com>
2025-08-24 00:23:19 +0800

    0823-C-package更新

--- src/pages/Package.jsx
+++ src/pages/Package.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import "../styles/_Package.scss"
 import c_package_twopeople from "../images/Regform_icon/c_package_twopeople.png"
+import c_package_00 from "../images/Regform_icon/c_package_00.png"
+import c_package_1 from "../images/Regform_icon/c_package_1.png"
+import c_package_2 from "../images/Regform_icon/c_package_2.png"
+import c_package_3 from "../images/Regform_icon/c_package_3.png"
+import c_package_4 from "../images/Regform_icon/c_package_4.png"
+import c_package_5 from "../images/Regform_icon/c_package_5.png"
+import c_package_6 from "../images/Regform_icon/c_package_6.png"
+import c_package_7 from "../images/Regform_icon/c_package_7.png"
+import c_package_8 from "../images/Regform_icon/c_package_8.png"
+import c_package_9 from "../images/Regform_icon/c_package_9.png"
+import c_package_10 from "../images/Regform_icon/c_package_10.png"
+import c_package_11 from "../images/Regform_icon/c_package_11.png"
+import c_package_12 from "../images/Regform_icon/c_package_12.png"
+import c_package_13 from "../images/Regform_icon/c_package_13.png"
 
 const Package = () => {
     return (
@@ -20,7 +34,28 @@ const Package = () => {
                     </div>
                 </section>
 
-                <div>我是下一段</div>
+                <div className='c_packPrepare'>
+                    <p className='c_packA'>開始包裝前，請先準備好這些材料！</p>
+                    <div className='c_packOutBox'>
+                        <div className='c_packBox'>
+                            <p className='c_packTitle'>堅固耐壓的紙箱</p>
+                            <p className='c_packDirections'>請使用尺寸適中、結構完整的紙箱。如果是回收紙箱，務必確保其未受損，並清除上面的舊標籤與條碼，避免造成物流混淆。</p>
+                        </div>
+                        <img src={c_package_1} alt="" />
+                        <img src={c_package_2} alt="" />
+                    </div>
+                    <div className='c_packOutBox'>
+                        <div className='c_packBox'>
+                            <div>
+                                <p className='c_packTitle'>內部緩衝包材</p>
+                            </div>
+                            <p className='c_packDirections'>您可以使用報紙、氣泡布、保麗龍粒、泡棉或碎紙等作為內襯。重點是物品在箱內不能晃動，四周要有緩衝空間，保護物品不被擠壓損壞。<br />若是易碎品，請單獨仔細包裹，並在外箱明確標示「易碎品」。</p>
+                        </div>
+                        <img src={c_package_3} alt="" />
+                        <img src={c_package_4} alt="" />
+                    </div>
+                </div>
+
             </main>
 
         </>
