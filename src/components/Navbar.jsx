7ae22d0ba3982513260a401e626fa5ebde82b56a
Thatisfish <roobin4321@gmail.com>
2025-09-03 14:46:03 +0800

    0903-f-navbar手機板製作完成

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,6 +54,30 @@ export default function Navbar({ theme, onOpenLogin }) {
 					</ul>
 				</nav>
 			</header>
+			<header className={`mobile-nav ${theme}`}>
+				<div className="mobile-nav__container">
+
+					<div className="mobile-icon">
+						<Link to="/#">
+							<img src={Logo} alt="" />
+						</Link>
+					</div>
+					<div className="mobile-nav__search">
+						<input
+							type="text"
+							placeholder="搜尋商品..."
+							className="mobile-nav__search-input"
+						/>
+						<div className="icon-search mobile-icon">
+							<Link to="/#" />
+						</div>
+					</div>
+					<div className="icon-hamburger mobile-icon">
+						<Link to="/HamburgerMenu" />
+					</div>
+
+				</div>
+			</header>
 			<nav className="mobile-bottom-nav">
 				<Link to="/"><img src={Home} alt="" />首頁</Link>
 				<Link to="/Notice"><img src={Bell} alt="" />通知</Link>
