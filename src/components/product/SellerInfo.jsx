f9b10ffa41510f5313a312d3c417d5b0cc721b91
Thatisfish <roobin4321@gmail.com>
2025-09-12 16:53:36 +0800

    0912-f-rwd改一半

--- src/components/product/SellerInfo.jsx
+++ src/components/product/SellerInfo.jsx
@@ -6,36 +6,58 @@ import user from '../../images/ProductPage/user.jpg'
 import star from '../../images/ProductPage/star.svg'
 const SellerInfo = () => {
 	return (
-		<div className="seller">
-			<div className="seller__profile">
+		<>
+			<div className="seller seller-pc">
+				<div className="seller__profile">
+					<img src={user} alt="賣家頭像" className="seller__avatar" />
+					<p className="seller__name">@fun31**56</p>
+				</div>
+				<div className='seller__rwd'>
+					<div className="seller__stats">
+						<div className="seller__rating">
+							<span className="seller__score">4.8</span>
+							<img src={star} alt="" />
+							<span className="seller__count">(5188則評價)</span>
+						</div>
+						<p className="seller__location">台北市/萬華區</p>
+						<div className="seller__tags">
+							<img src={check} alt="" />
+							<img src={fastShip} alt="" />
+							<img src={quickReply} alt="" />
+						</div>
+					</div>
+					<div className="seller__desc">
+						<p>
+							個人賣場，商品多九成新，基本上24小時內回覆，歡迎使用留言提問及聊聊^^
+						</p>
+					</div>
+				</div>
+				<div className="seller__actions">
+					<button className="btn">追蹤+</button>
+					<button className="btn">聊聊</button>
+				</div>
+			</div>
+			<div className="seller seller-mobile">
 				<img src={user} alt="賣家頭像" className="seller__avatar" />
-				<p className="seller__name">@fun31**56</p>
-			</div>
-
-			<div className="seller__stats">
-				<div className="seller__rating">
-					<span className="seller__score">4.8</span>
-					<img src={star} alt="" />
-					<span className="seller__count">(5188則評價)</span>
+				<div className="scenter">
+					<p className="seller__name">@fun31**56</p>
+					<div className="seller__rating">
+						<span className="seller__score">4.8</span>
+						<img src={star} alt="" />
+						<span className="seller__count">(5188則評價)</span>
+					</div>
+					<div className="seller__tags">
+						<img src={check} alt="" />
+						<img src={fastShip} alt="" />
+						<img src={quickReply} alt="" />
+					</div>
 				</div>
-				<p className="seller__location">台北市/萬華區</p>
-				<div className="seller__tags">
-					<img src={check} alt="" />
-					<img src={fastShip} alt="" />
-					<img src={quickReply} alt="" />
+				<div className="seller__actions">
+					<button className="btn">追蹤+</button>
+					<button className="btn">聊聊</button>
 				</div>
 			</div>
-			<div className="seller__desc">
-				<p>
-					個人賣場，商品多九成新，基本上24小時內回覆，歡迎使用留言提問及聊聊^^
-				</p>
-			</div>
-			<div className="seller__actions">
-				<button className="btn">追蹤+</button>
-				<button className="btn">聊聊</button>
-			</div>
-		</div>
-
+		</>
 	)
 }
 
