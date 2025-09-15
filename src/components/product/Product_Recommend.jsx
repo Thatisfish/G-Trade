62e989f9bf9e90140f29e781b579c5d356b890ef
Thatisfish <roobin4321@gmail.com>
2025-09-16 02:27:02 +0800

    0916-f-商品詳細頁面持續更動

--- src/components/product/Product_Recommend.jsx
+++ src/components/product/Product_Recommend.jsx
@@ -4,127 +4,37 @@ import DKB from '../../images/Card_Image/DKB.webp'
 import DS2CE from '../../images/Card_Image/DS2CE.webp'
 import ps5_pro from '../../images/Card_Image/ps5_pro.webp'
 import SWP2 from '../../images/Card_Image/switch_pro2.webp'
-import UserImg from '../../images/ProductPage/user.jpg'
+import UImg from '../../images/ProductPage/user.jpg'
 
-const PrCard = () => {
+const PrCard = ({ userImg, userName, upDate, commodityImage, commodityTitle, commodityPrice }) => {
 	const [isFav, setIsFav] = useState(false);
+
 	return (
 		<div className="abc">
-			<div className='o1'>
-				<div className='o2'>
-					<div className='cain'>
-						<a href="#" className='userInfo'>
+			<div className="o1">
+				<div className="o2">
+					<div className="cain">
+						<a href="#" className="userInfo">
 							<div>
-								<img src={UserImg} alt="" className='userImg' />
+								<img src={userImg} alt="" className="userImg" />
 							</div>
 							<div>
-								<p className='userName'>paly**56</p>
-								<p className='upDate'>3天前</p>
+								<p className="userName">{userName}</p>
+								<p className="upDate">{upDate}</p>
 							</div>
 						</a>
 
-						<a href="" className='commodityInfo'>
-							<div className='comImg'>
-								<img src={DKB} alt="" />
+						<a href="" className="commodityInfo">
+							<div className="comImg">
+								{/* {tag && <div className="info-tag">全新上架</div>} */}
+								<img src={commodityImage} alt="" />
 							</div>
-							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
-							<p className='comPrice'>NT$800</p>
+							<p className="comTitle">{commodityTitle}</p>
+							<p className="comPrice">NT${commodityPrice}</p>
 						</a>
 					</div>
-					<div className='comHeart'>
-						<button
-							className={`btn-fav ${isFav ? "active" : ""}`}
-							onClick={() => setIsFav(!isFav)}
-						>
-							<span className="heart"></span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div className='o1'>
-				<div className='o2'>
-					<div className='cain'>
-						<a href="#" className='userInfo'>
-							<div>
-								<img src={UserImg} alt="" className='userImg' />
-							</div>
-							<div>
-								<p className='userName'>paly**56</p>
-								<p className='upDate'>3天前</p>
-							</div>
-						</a>
 
-						<a href="" className='commodityInfo'>
-							<div className='comImg'>
-								<img src={DKB} alt="" />
-							</div>
-							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
-							<p className='comPrice'>NT$800</p>
-						</a>
-					</div>
-					<div className='comHeart'>
-						<button
-							className={`btn-fav ${isFav ? "active" : ""}`}
-							onClick={() => setIsFav(!isFav)}
-						>
-							<span className="heart"></span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div className='o1'>
-				<div className='o2'>
-					<div className='cain'>
-						<a href="#" className='userInfo'>
-							<div>
-								<img src={UserImg} alt="" className='userImg' />
-							</div>
-							<div>
-								<p className='userName'>paly**56</p>
-								<p className='upDate'>3天前</p>
-							</div>
-						</a>
-
-						<a href="" className='commodityInfo'>
-							<div className='comImg'>
-								<img src={DKB} alt="" />
-							</div>
-							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
-							<p className='comPrice'>NT$800</p>
-						</a>
-					</div>
-					<div className='comHeart'>
-						<button
-							className={`btn-fav ${isFav ? "active" : ""}`}
-							onClick={() => setIsFav(!isFav)}
-						>
-							<span className="heart"></span>
-						</button>
-					</div>
-				</div>
-			</div>
-			<div className='o1'>
-				<div className='o2'>
-					<div className='cain'>
-						<a href="#" className='userInfo'>
-							<div>
-								<img src={UserImg} alt="" className='userImg' />
-							</div>
-							<div>
-								<p className='userName'>paly**56</p>
-								<p className='upDate'>3天前</p>
-							</div>
-						</a>
-
-						<a href="" className='commodityInfo'>
-							<div className='comImg'>
-								<img src={DKB} alt="" />
-							</div>
-							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
-							<p className='comPrice'>NT$800</p>
-						</a>
-					</div>
-					<div className='comHeart'>
+					<div className="comHeart">
 						<button
 							className={`btn-fav ${isFav ? "active" : ""}`}
 							onClick={() => setIsFav(!isFav)}
@@ -138,59 +48,58 @@ const PrCard = () => {
 	)
 }
 
+// 多張卡片
 const Inforcard = () => {
 	const arrCardinfor = [
 		{
 			id: 1,
-			// tag: '全新上架',
-			image: DKB,
-			title: '咚奇剛蕉力全開+咚奇剛amiibo',
-			seller: 'paly**56',
-			priceNow: '800',
-			size: 'small'
+			userImg: UImg,
+			userName: 'paly**56',
+			upDate: '3天前',
+			commodityImage: DKB,
+			commodityTitle: '咚奇剛蕉力全開+咚奇剛amiibo',
+			commodityPrice: '800',
 		},
 		{
 			id: 2,
-			tag: '全新上架',
-			image: DS2CE,
-			title: '死亡擱淺典藏版',
-			seller: 'Ds2CE**250626',
-			priceNow: '1100',
-			size: 'medium'
+			userImg: UImg,
+			userName: 'Ds2CE**250626',
+			upDate: '3天前',
+			commodityImage: DS2CE,
+			commodityTitle: '死亡擱淺典藏版',
+			commodityPrice: '1100',
 		},
 		{
 			id: 3,
-			tag: '限時優惠',
-			image: ps5_pro,
-			title: 'PS5 PRO主機 1TB 極致黑',
-			seller: 'Ssp**5',
-			priceNow: '9999',
-			priceOld: '15000',
-			size: 'large'
+			userImg: UImg,
+			userName: 'Ssp**5',
+			upDate: '3天前',
+			commodityImage: ps5_pro,
+			commodityTitle: 'PS5 PRO主機 1TB 極致黑',
+			commodityPrice: '9999',
 		},
 		{
 			id: 4,
-			tag: '限時優惠',
-			image: SWP2,
-			title: 'PS5 PRO主機 1TB 極致黑',
-			seller: 'fun31**56',
-			priceNow: '5000',
-			priceOld: '5500',
-			size: 'medium'
+			userImg: UImg,
+			userName: 'fun31**56',
+			upDate: '3天前',
+			commodityImage: SWP2,
+			commodityTitle: 'Switch 2全新主機',
+			commodityPrice: '5000',
 		},
 	]
+
 	return (
-		<div className="container">
-			{arrCardinfor.map((cardinfor) => (
-				<Card
-					key={cardinfor.id}
-					tag={cardinfor.tag}
-					image={cardinfor.image}
-					title={cardinfor.title}
-					seller={cardinfor.seller}
-					priceNow={cardinfor.priceNow}
-					priceOld={cardinfor.priceOld}
-					size={cardinfor.size}
+		<div className="containerPrCard">
+			{arrCardinfor.map((card) => (
+				<PrCard
+					key={card.id}
+					userImg={card.userImg}
+					userName={card.userName}
+					upDate={card.upDate}
+					commodityImage={card.commodityImage}
+					commodityTitle={card.commodityTitle}
+					commodityPrice={card.commodityPrice}
 				/>
 			))}
 		</div>
@@ -201,8 +110,8 @@ const ProductRecommend = () => {
 	return (
 		<div className='recommend'>
 			<p className='recommend_title'>你可能有興趣的商品</p>
-			{/* <Inforcard /> */}
-			<PrCard />
+			<Inforcard />
+			{/* <PrCard /> */}
 			<div className='recommend_btn'>
 				<button className='btn'>更多商品</button>
 			</div>
