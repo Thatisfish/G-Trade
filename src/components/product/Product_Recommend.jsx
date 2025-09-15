86dd347f1d50bad6772ce34d1151b10364f198b7
Thatisfish <roobin4321@gmail.com>
2025-09-15 17:20:27 +0800

    0915-f-商品詳細頁持續修改

--- src/components/product/Product_Recommend.jsx
+++ src/components/product/Product_Recommend.jsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import { useState } from "react";
 import Card from '../Card'
 import DKB from '../../images/Card_Image/DKB.webp'
 import DS2CE from '../../images/Card_Image/DS2CE.webp'
 import ps5_pro from '../../images/Card_Image/ps5_pro.webp'
 import SWP2 from '../../images/Card_Image/switch_pro2.webp'
+import UserImg from '../../images/ProductPage/user.jpg'
 
+const PrCard = () => {
+	const [isFav, setIsFav] = useState(false);
+	return (
+		<div className="abc">
+			<div className='o1'>
+				<div className='o2'>
+					<div className='cain'>
+						<a href="#" className='userInfo'>
+							<div>
+								<img src={UserImg} alt="" className='userImg' />
+							</div>
+							<div>
+								<p className='userName'>paly**56</p>
+								<p className='upDate'>3天前</p>
+							</div>
+						</a>
 
+						<a href="" className='commodityInfo'>
+							<div className='comImg'>
+								<img src={DKB} alt="" />
+							</div>
+							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
+							<p className='comPrice'>NT$800</p>
+						</a>
+					</div>
+					<div className='comHeart'>
+						<button
+							className={`btn-fav ${isFav ? "active" : ""}`}
+							onClick={() => setIsFav(!isFav)}
+						>
+							<span className="heart"></span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className='o1'>
+				<div className='o2'>
+					<div className='cain'>
+						<a href="#" className='userInfo'>
+							<div>
+								<img src={UserImg} alt="" className='userImg' />
+							</div>
+							<div>
+								<p className='userName'>paly**56</p>
+								<p className='upDate'>3天前</p>
+							</div>
+						</a>
+
+						<a href="" className='commodityInfo'>
+							<div className='comImg'>
+								<img src={DKB} alt="" />
+							</div>
+							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
+							<p className='comPrice'>NT$800</p>
+						</a>
+					</div>
+					<div className='comHeart'>
+						<button
+							className={`btn-fav ${isFav ? "active" : ""}`}
+							onClick={() => setIsFav(!isFav)}
+						>
+							<span className="heart"></span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className='o1'>
+				<div className='o2'>
+					<div className='cain'>
+						<a href="#" className='userInfo'>
+							<div>
+								<img src={UserImg} alt="" className='userImg' />
+							</div>
+							<div>
+								<p className='userName'>paly**56</p>
+								<p className='upDate'>3天前</p>
+							</div>
+						</a>
+
+						<a href="" className='commodityInfo'>
+							<div className='comImg'>
+								<img src={DKB} alt="" />
+							</div>
+							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
+							<p className='comPrice'>NT$800</p>
+						</a>
+					</div>
+					<div className='comHeart'>
+						<button
+							className={`btn-fav ${isFav ? "active" : ""}`}
+							onClick={() => setIsFav(!isFav)}
+						>
+							<span className="heart"></span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className='o1'>
+				<div className='o2'>
+					<div className='cain'>
+						<a href="#" className='userInfo'>
+							<div>
+								<img src={UserImg} alt="" className='userImg' />
+							</div>
+							<div>
+								<p className='userName'>paly**56</p>
+								<p className='upDate'>3天前</p>
+							</div>
+						</a>
+
+						<a href="" className='commodityInfo'>
+							<div className='comImg'>
+								<img src={DKB} alt="" />
+							</div>
+							<p className='comTitle'>咚奇剛蕉力全開+咚奇剛amiibo</p>
+							<p className='comPrice'>NT$800</p>
+						</a>
+					</div>
+					<div className='comHeart'>
+						<button
+							className={`btn-fav ${isFav ? "active" : ""}`}
+							onClick={() => setIsFav(!isFav)}
+						>
+							<span className="heart"></span>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 const Inforcard = () => {
 	const arrCardinfor = [
@@ -70,7 +201,8 @@ const ProductRecommend = () => {
 	return (
 		<div className='recommend'>
 			<p className='recommend_title'>你可能有興趣的商品</p>
-			<Inforcard />
+			{/* <Inforcard /> */}
+			<PrCard />
 			<div className='recommend_btn'>
 				<button className='btn'>更多商品</button>
 			</div>
