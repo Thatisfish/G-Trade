4c604e7125d9fc58b14f762ae27c5874ddf95165
achi9444 <shark1997329@gmail.com>
2025-09-16 13:25:12 +0800

    0916-C-new_info_完

--- src/pages/New_info.jsx
+++ src/pages/New_info.jsx
@@ -1,9 +1,19 @@
+import HomeNews from '../components/HomeNewsSection'
+
 import '../styles/New_info.scss'
 const New_info = () => {
   return (
-    <main id='New_info'>
-      <div>我是最新消息</div>
-    </main>
+    <>
+      <main id='New_info'>
+        <header className='c_newHead'>
+          <h2>最新消息</h2>
+          <p>NEWS</p>
+        </header>
+        <div className='c_newNewBox'>
+          <HomeNews />
+        </div>
+      </main>
+    </>
   )
 }
 
