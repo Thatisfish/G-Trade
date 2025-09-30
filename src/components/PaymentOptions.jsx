09b27b01637b70e8415bfff5cb537f859c711846
maichu59 <withlove13441@gmail.com>
2025-09-30 10:50:03 +0800

    0930-J-購物車修改

--- src/components/PaymentOptions.jsx
+++ src/components/PaymentOptions.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import visa from '../images/ShoppingCard_icon/visa.svg'
 import visa2 from '../images/ShoppingCard_icon/visa2.svg'
 import JCB from '../images/ShoppingCard_icon/JCB.svg'
+import LINEPay from '../images/ShoppingCard_icon/LINEPay-2.png'
+import JKOPay from '../images/ShoppingCard_icon/JKOPay-2.png'
 
 export default function PaymentOptions() {
   return (
     <div className="J_paymentOptions">
       <label>
-        <input type="radio" name="paym  ent" value="credit" />
+        <input type="radio" name="payment" value="credit" />
         信用卡
         <img src={visa} alt="visa" />
         <img src={visa2} alt="master" />
@@ -24,6 +26,14 @@ export default function PaymentOptions() {
         <input type="radio" name="payment" value="cvs" />
         超商貨到付款
       </label>
+
+      <label>
+        <input type="radio" name="payment" value="MobilePayment" />
+        電子支付
+        <img className="J_LINEPAY" src={LINEPay} alt="LINEPay" />
+        <img className="J_JKOPAY" src={JKOPay} alt="JKOPay" />
+      </label>
+
     </div>
   );
 }
